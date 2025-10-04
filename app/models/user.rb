8d72b6b9f58c0966b51abc716152ec3bba1cc9b3
Matheus <matheusslcosta@usp.br>
2025-10-03 23:40:41 -0300

    update pagina de cadatro

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :nome, presence: true
+  validates :password, confirmation: true, length: { minimum: 6 }
 end
