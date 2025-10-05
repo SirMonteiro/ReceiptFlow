33988cfc7b68380b4c371432d277b66045608088
Matheus <matheusslcosta@usp.br>
2025-10-05 20:09:22 -0300

    remove invalid migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_05_113337) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_05_140850) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,4 +63,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_05_113337) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
