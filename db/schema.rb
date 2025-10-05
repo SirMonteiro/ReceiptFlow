28706d0e76f56bcf3ce82c7ff345ef5807f4ac59
Gabriela Pinheiro <100321468+gabi-pinheiro@users.noreply.github.com>
2025-10-05 20:36:24 +0000

    retirada migration redundante

--- db/schema.rb
+++ db/schema.rb
@@ -56,4 +56,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_05_113337) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password", null: false
+    t.string "role", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
 end
