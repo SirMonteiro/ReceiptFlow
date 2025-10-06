dfab5897eb15f6f59893f44e119ea432b46095ae
Matheus <matheusslcosta@usp.br>
2025-10-05 21:59:58 -0300

    update user migration files

--- db/migrate/20251005140850_remove_streak_credito_nivel_from_users.rb
+++ db/migrate/20251005140850_remove_streak_credito_nivel_from_users.rb
@@ -1,7 +1,7 @@
 class RemoveStreakCreditoNivelFromUsers < ActiveRecord::Migration[7.1]
   def change
-    # remove_column :users, :streak, :integer
-    # remove_column :users, :credito, :integer
-    # remove_column :users, :nivel, :integer
+    remove_column :users, :streak, :integer
+    remove_column :users, :credito, :integer
+    remove_column :users, :nivel, :integer
   end
 end
