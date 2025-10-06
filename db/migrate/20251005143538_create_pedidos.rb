35c8ef92d7d040fd1cf3772bab6beaa89c77239f
Matheus <matheusslcosta@usp.br>
2025-10-05 23:14:01 -0300

    fix problems with migrations

--- db/migrate/20251005143538_create_pedidos.rb
+++ db/migrate/20251005143538_create_pedidos.rb
@@ -1,6 +1,6 @@
 class CreatePedidos < ActiveRecord::Migration[7.1]
   def change
-    create_table :pedidos do |t|
+    create_table :pedidos, if_not_exists: true do |t|
       t.string :cliente
       t.decimal :valor
 
