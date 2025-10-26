cf97faf1345738d36361b4695668af217c05047e
Matheus <matheusslcosta@usp.br>
2025-10-26 11:50:03 -0300

    add alert message to insert a valid password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :nome, presence: true
-  validates :password, confirmation: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6, message: "deve ter pelo menos 6 caracteres" }
 end
