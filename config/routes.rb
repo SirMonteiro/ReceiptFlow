e22a9419d2540304d83f7689d97a4f6ec59922c0
Ricardo Miranda <ricomcfilho@gmail.com>
2025-10-30 18:15:46 -0300

    feat: add controlador e view de controle financeiro

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,22 @@ Rails.application.routes.draw do
   # Rota para gráficos
   resources :graficos, only: [:index]
   
+  # Rotas para Controle Financeiro (metas, orçamento e despesas)
+  get 'controle_financeiro', to: 'controle_financeiro#index'
+
+  post 'controle_financeiro/metas', to: 'controle_financeiro#criar_meta'
+  patch 'controle_financeiro/metas/:id', to: 'controle_financeiro#atualizar_meta', as: :atualizar_meta
+  delete 'controle_financeiro/metas/:id', to: 'controle_financeiro#deletar_meta', as: :deletar_meta
+
+  post 'controle_financeiro/orcamentos', to: 'controle_financeiro#criar_orcamento'
+  patch 'controle_financeiro/orcamentos/:id', to: 'controle_financeiro#atualizar_orcamento', as: :atualizar_orcamento
+  delete 'controle_financeiro/orcamentos/:id', to: 'controle_financeiro#deletar_orcamento', as: :deletar_orcamento
+
+  post 'controle_financeiro/despesas', to: 'controle_financeiro#criar_despesa'
+  delete 'controle_financeiro/despesas/:id', to: 'controle_financeiro#deletar_despesa', as: :deletar_despesa
+
+
+
   # Rotas para faturamento
   resources :faturamento, only: [:index] do
     collection do
