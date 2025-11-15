d9702f95813d586e34e96aa94908a5f73a998b70
Ricardo Miranda <ricomcfilho@gmail.com>
2025-11-15 20:33:41 -0300

    feat: add controller and spec tests to Gráficos

--- app/controllers/graficos_controller.rb
+++ app/controllers/graficos_controller.rb
@@ -1,30 +1,36 @@
 class GraficosController < ApplicationController
   before_action :require_login, only: [:index]
   def index
-    notas = NotaGrafico.all
-    despesas = Despesa.all
-    meta = MetaMensal.last
+    @user = current_user
 
-    if notas.empty?
-      @nenhum_dado = true
-      return
-    end
+    # serão usados no gráfico de pizza
+    @mes_atual = Date.today.month
+    @ano_atual = Date.today.year
+    @meta_atual = @user.metas_mensais.find_by(mes: @mes_atual, ano: @ano_atual) 
 
-    # Cenário: Exibir gráfico de vendas mensais (gráfico de barras)
-    @ganhos_mensais = notas.group_by { |n| n.data.strftime("%B") }
-                           .transform_values { |ns| ns.sum(&:valor) }
 
-    # Cenário: Exibir gráfico de despesas/ganhos (gráfico de barras duplas)
-    @lucros = @ganhos_mensais
-    @despesas = despesas.group_by { |d| d.data.strftime("%B") }
-                        .transform_values { |ds| ds.sum(&:valor) }
+    # vendas nos últimos 6 meses (contando com o mês atual)
+    @vendas = @user.danfes
+              .where("natureza_operacao ILIKE ?", 'venda')
+              .where("data_saida >= ?", 5.months.ago.beginning_of_month) # como que isso existe
+              .select("DATE_TRUNC('month', data_saida) AS mes_ano, SUM(valor) AS total")
+              .group("mes_ano")
+              .order("mes_ano ASC")
+              # look at my ruby daaaawg ts just sql :(
 
-    # Cenário: Exibir gráfico de vendas/meta (gráfico de pizza)
-    total_arrecadado = notas.sum(&:valor)
-    if meta.present?
-      @vendas_meta = { "Arrecadado" => total_arrecadado, "Meta" => meta.valor_meta }
-    else
-      @vendas_meta = nil
-    end
+    # despesas nos últimos 6 meses
+    @despesas = @user.despesas
+                .where("data >= ?", 5.months.ago.beginning_of_month)
+                .select("DATE_TRUNC('month', data) AS mes_ano, SUM(valor) AS total")
+                .group("mes_ano")
+                .order("mes_ano ASC")
+    
+    # últimas 6 metas (verificação de datas deve ser feita posteriormente)
+    @metas = @user.metas_mensais
+              .order(ano: :desc, mes: :desc)
+              .limit(6)
+
   end
 end
+
+
---
+++ spec/controllers/graficos_controller_spec.rb
@@ -0,0 +1,83 @@
+require 'rails_helper'
+
+RSpec.describe GraficosController, type: :controller do
+  describe "GET #index" do
+
+    before do
+      @user = User.create!(
+        email: "teste@teste.com",
+        nome: "Usuário Teste",
+        password: "123456"
+      )
+
+      allow_any_instance_of(ApplicationController)
+        .to receive(:current_user)
+        .and_return(@user)
+    end
+
+    context "com dados de vendas, despesas e metas" do
+      before do
+        (0..6).each do |i|
+            Danfe.create!(
+              user: @user,
+              cliente: "Dr.Strangelove",
+              valor: 100 * (i + 1),
+              chave_acesso: "CHAVE#{i}",
+              natureza_operacao: "venda",
+              remetente: "Stanley Kubrick",
+              destinatario: "Destinatarilson",
+              descricao_produtos: "doomsday machine ahh produtos",
+              valores_totais: 100 * (i + 1),
+              impostos: 0,
+              cfop: "5102",
+              cst: "00",
+              ncm: "12345678",
+              transportadora: "Transportadora",
+              data_saida: (i).months.ago.beginning_of_month
+            )
+        end
+
+        (0..6).each do |i|
+            Despesa.create!(
+              user: @user,
+              valor: 50 * (i + 1),
+              data: (i).months.ago.beginning_of_month
+            )
+        end
+
+       (0..6).each do |i|
+            MetaMensal.create!(
+              user: @user,
+              valor_meta: 10 * (i + 1),
+              mes: (11 - i),
+              ano: 2025
+            )
+        end
+
+        get :index
+      end
+
+      it "pega somente as danfes dos últimos 6 meses" do
+        expect(assigns(:vendas).length).to eq(6)
+      end
+
+      it "soma corretamente os valores das vendas" do
+        totais = assigns(:vendas).map { |v| v.total.to_f }.sort
+        expect(totais).to eq([100, 200, 300, 400, 500, 600])
+      end
+
+      it "pega somente as despesas dos últimos 6 meses" do
+        expect(assigns(:despesas).length).to eq(6)
+      end
+
+      it "soma corretamente os valores das despesas" do
+        totais = assigns(:despesas).map { |d| d.total.to_f }.sort
+        expect(totais).to eq([50, 100, 150, 200, 250, 300])
+      end
+
+      it "pega somente as 6 metas mais recentes" do
+        expect(assigns(:metas).length).to eq(6)
+      end
+    end
+  end
+end
