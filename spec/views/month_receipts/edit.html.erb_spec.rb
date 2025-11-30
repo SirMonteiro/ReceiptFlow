f6c422637de3e67cf4577e682315692d4c96493c
Gabriel de Souza <gabrielmonteirodesouza@tutanota.com>
2025-11-30 14:47:07 -0300

    test: fix month_receipts and uploads

--- spec/views/month_receipts/edit.html.erb_spec.rb
+++ spec/views/month_receipts/edit.html.erb_spec.rb
@@ -1,20 +1,4 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
-
-RSpec.describe 'month_receipts/edit', type: :view do
-  let(:month_receipt) do
-    MonthReceipt.create!
-  end
-
-  before do
-    assign(:month_receipt, month_receipt)
-  end
-
-  it 'renders the edit month_receipt form' do
-    render
-
-    assert_select 'form[action=?][method=?]', month_receipt_path(month_receipt), 'post' do
-    end
-  end
-end
+# This spec is intentionally left blank because MonthReceipts no longer exposes
+# edit functionality.
--- spec/views/month_receipts/new.html.erb_spec.rb
+++ spec/views/month_receipts/new.html.erb_spec.rb
@@ -1,16 +1,4 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
-
-RSpec.describe 'month_receipts/new', type: :view do
-  before do
-    assign(:month_receipt, MonthReceipt.new)
-  end
-
-  it 'renders new month_receipt form' do
-    render
-
-    assert_select 'form[action=?][method=?]', month_receipts_path, 'post' do
-    end
-  end
-end
+# This spec is intentionally left blank because MonthReceipts no longer supports
+# the scaffolded `new` view.
